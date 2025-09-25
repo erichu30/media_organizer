8240168f54ef0f64b2cf37024d4da23c4a0a0853
erichu30 <erichu@erichu.cc>
2025-09-25 11:42:22 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 mkdir -p build
 
 if ! command -v go &> /dev/null; then
-  echo "Go command not found. Activating with 'gvm use go1.25.0'"
+  if ! command -v gvm &> /dev/null; then
+    echo "Go command not found and gvm is not installed. Please install Go or gvm."
+    exit 1
+  fi
+  echo "Go command not found. Activating with gvm"
+  gvm install go1.25.0
   gvm use go1.25.0
 fi
 
