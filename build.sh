40a8e0b6713462942274cb400148438123815dcf
erichu30 <erichu@erichu.cc>
2025-09-25 11:32:02 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 mkdir -p build
+
+if ! command -v go &> /dev/null; then
+  echo "Go command not found. Activating with 'gvm use go1.25.0'"
+  gvm use go1.25.0
+fi
+
 go build -o ./build/sort_by_date src/cmd/sort_by_date.go
